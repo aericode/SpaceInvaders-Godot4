951d404cd17bfe1e90787ca24f42034210371543
Eric Andrade <e.andrade5f@gmail.com>
2023-03-14 19:05:58 -0300

    single bullet per screen rule implemented

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,7 +7,7 @@ var bullet_resource = preload("res://resources/bullet.tscn")
 @export var player_limit_begin = 0
 @export var player_limit_end = 0
 
-var shot_cooldown = 0
+var bullet = null
 
 func handle_movement():
 	var input = 0
@@ -18,29 +18,17 @@ func handle_movement():
 	position.x = clamp(position.x + input * speed, player_limit_begin, player_limit_end)
 
 func spawn_bullet():
-	var bullet = bullet_resource.instantiate()
+	bullet = bullet_resource.instantiate()
 	bullet.position = get_position() + Vector2(0,-5)
 	get_node("/root/World").add_child(bullet)
 
-func set_cooldown():
-	var cooldown_reset_value = 30
-	shot_cooldown = cooldown_reset_value
-
-func decrease_cooldown():
-	var cooldown_decrease_value = 1
-	if(shot_cooldown < 0):
-		shot_cooldown = 0
-	elif(shot_cooldown > 0):
-		shot_cooldown -= cooldown_decrease_value
+func can_shoot():
+	return bullet == null
 	
 func handle_shooting():
-	if (Input.is_action_just_pressed("shoot") && shot_cooldown <= 0):
+	if (Input.is_action_just_pressed("shoot") && can_shoot()):
 		spawn_bullet()
-		set_cooldown()
 
 func _process(delta):
 	handle_movement()
 	handle_shooting()
-	decrease_cooldown()
-	
-		
