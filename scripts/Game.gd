e63d7ddc67f625127aebd3657066e83c8df5ea8b
Eric Andrade <e.andrade5f@gmail.com>
2023-03-13 23:09:43 -0300

    raising vertical offset
    offset is now 20

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -4,19 +4,26 @@ var enemy_resource = preload("res://enemy.tscn")
 
 func instantiate_enemy(position:Vector2, line_index):
 	var enemy = enemy_resource.instantiate()
+	var enemy_sprite
+	
 	enemy.set_position(position)
 	if(line_index == 1):
 		enemy.score_value =  40
+		enemy_sprite = load("res://assets/red.png")
 	if(line_index == 2 || line_index == 3):
 		enemy.score_value =  20
+		enemy_sprite = load("res://assets/yellow.png")
 	if(line_index == 4 || line_index == 5):
 		enemy.score_value =  10
+		enemy_sprite = load("res://assets/green.png")
+	
+	enemy.get_node("Sprite2D").texture = enemy_sprite
 	
 	add_child(enemy);
 
 func instantiate_line(level):
-	var vertical_offset = 10
-	var horizontal_offset = 385
+	var vertical_offset = 20
+	var horizontal_offset = 380
 	var horizontal_limit = 800
 	
 	var side_spacing = 50
@@ -26,7 +33,7 @@ func instantiate_line(level):
 		instantiate_enemy(Vector2(x_pos, height), level)
 		
 func instantiatee_swarm():
-	var number_of_lines = 1
+	var number_of_lines = 5
 	for level in range(1,number_of_lines+1,1):
 		instantiate_line(level)
 
