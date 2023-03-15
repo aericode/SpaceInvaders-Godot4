203c3085796e01de4dfcacbfdbcd2f9bcded2461
Eric Andrade <e.andrade5f@gmail.com>
2023-03-15 11:45:30 -0300

    enemy destruction
    on any collision

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -6,7 +6,13 @@ extends Area2D
 func _ready():
 	pass # Replace with function body.
 
+func destroy_self():
+	queue_free()
+
+func detect_hit():
+	if(has_overlapping_areas()):
+		destroy_self()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	detect_hit()
