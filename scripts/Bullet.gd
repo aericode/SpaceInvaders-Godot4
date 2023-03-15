65fbc75270eaf4751895cdbc78b57a79b53ed145
Eric Andrade <e.andrade5f@gmail.com>
2023-03-15 01:16:06 -0300

    bullet destruction

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 @export var is_enemy:bool = false
 var move_direction
@@ -9,4 +9,5 @@ func _ready():
 
 func _physics_process(delta):
 	position += Vector2(0,-10)
-
+	if(has_overlapping_areas()):
+		queue_free()
