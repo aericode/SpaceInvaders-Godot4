52532fa27ae9bb11d26b0ada845269d290970b68
Eric Andrade <e.andrade5f@gmail.com>
2023-03-18 00:57:40 -0300

    removing debug script from swarm

--- Swarm.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-func _process(delta):
-	position += Vector2(1,0)
