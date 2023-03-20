6db13b68f798f9c91d10d66ee9b4fd0f9486258f
Eric Andrade <e.andrade5f@gmail.com>
2023-03-20 19:20:16 -0300

    instancing full shield

--- scripts/Shields.gd
+++ scripts/Shields.gd
@@ -17,8 +17,17 @@ func instantiate_full_line(line_origin_position:Vector2):
 	for brick_index in 4:
 		instantiate_brick( get_brick_position_in_line(line_origin_position, brick_index) )
 
-func instantiate_half_line(origin_position:Vector2):
-	instantiate_brick( get_brick_position_in_line(origin_position, 0) )
-	instantiate_brick( get_brick_position_in_line(origin_position, 3) )
+func instantiate_half_line(line_origin_position:Vector2):
+	instantiate_brick( get_brick_position_in_line(line_origin_position, 0) )
+	instantiate_brick( get_brick_position_in_line(line_origin_position, 3) )
 
+func get_line_origin_position(shield_origin_position:Vector2, line_index):
+	var brick_height = 20
+	return Vector2(shield_origin_position.x, shield_origin_position.y + line_index*brick_height)
+	
+func instantiate_shield(shield_origin_position:Vector2):
+	instantiate_full_line(get_line_origin_position(shield_origin_position, 0))
+	instantiate_full_line(get_line_origin_position(shield_origin_position, 1))
+	instantiate_half_line(get_line_origin_position(shield_origin_position, 2))
+	
 	
