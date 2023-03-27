5e8e4b9c90c11bd3022af1600f8195ad4faa487f
Eric Andrade <e.andrade5f@gmail.com>
2023-03-27 11:46:38 -0300

    created the player_controller node to handle player instantiation

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,18 +2,10 @@ extends Node2D
 
 @onready var Swarm = get_node("Swarm")
 @onready var Shields = get_node("Shields")
+@onready var Player_controller = get_node("Player_controller")
 
-func instantiate_player():
-	var player_resource = preload("res://resources/Player.tscn")
-	var player = player_resource.instantiate()
-	player.position = Vector2(580, 615)
-	add_child(player)
-
-
-func _process(_delta):
-	pass
 	
 func _ready():
+	Player_controller.instantiate_player()
 	Swarm.instantiate_swarm()
 	Shields.instantiate_shield_set()
-	instantiate_player()
