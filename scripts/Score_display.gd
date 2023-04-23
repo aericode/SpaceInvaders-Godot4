74ab789876462242221b977cc60dc761380c06f0
Eric Andrade <e.andrade5f@gmail.com>
2023-04-23 15:21:20 -0300

    feat: variable controlling display of lives for gameover screen

--- scripts/Score_display.gd
+++ scripts/Score_display.gd
@@ -1,4 +1,9 @@
 extends Label
 
+@export var isDisplayLives:bool;
+
 func _process(_delta):
-	self.text = "SCORE" + "\n" + str(Global.score) + "\n\n" + "LIVES" + "\n" + str(Global.lives)
+	var lives_display_text = ""
+	if(isDisplayLives):
+		lives_display_text = "\n\n" + "LIVES" + "\n" + str(Global.lives);
+	self.text = "SCORE" + "\n" + str(Global.score) + lives_display_text + "\n\n" + "LEVEL" + "\n" + str(Global.level)
