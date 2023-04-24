193e9de766f4aa8a0f9cd04b3cbe00758d7c11b1
Eric Andrade <e.andrade5f@gmail.com>
2023-04-24 11:20:08 -0300

    wip: loading hi-scores from storage on main menu
    creating file if no file is available

--- scripts/Data_controller.gd
+++ scripts/Data_controller.gd
@@ -1,18 +1,27 @@
 extends Node2D
 
 var save_path = "user://savegame.save" #place of the file
-var dict = {}
 
 func load_scores():
 	var file = FileAccess.open(save_path, FileAccess.READ)
 	var content = file.get_as_text()
-	var content_json =  dict.parse_json(content)
-	return content
+	var content_json =  Global.hi_scores.parse_json(content)
+	file.close()
+	
+func check_if_scores_are_loaded():
+	if(Global.hi_scores == []):
+		load_scores()
 
+func create_save():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var([])
+	file.close()
 
+func check_if_file_exists():
+	if (not FileAccess.file_exists(save_path)):
+		create_save()
+		
 func _ready():
-	load_scores()
+	check_if_file_exists()
+	create_save()
 
-
-func _process(delta):
-	pass
