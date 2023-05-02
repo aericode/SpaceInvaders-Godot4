b01c69f4c1a160b80729f743ad31afe9014b527f
Eric Andrade <e.andrade5f@gmail.com>
2023-05-01 21:03:26 -0300

    feat: main menu hi-score display

--- Main_menu.gd
+++ Main_menu.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 @onready var Hi_score_manager = get_node("Hi_score_manager")
+@onready var Hi_score_display = get_node("Hi_score_display")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Hi_score_manager.handle_save_file_exists()
 	Hi_score_manager.handle_scores_loaded()
+	Hi_score_display.text = "[center]" + str(Global.hi_score) +"[/center]"
