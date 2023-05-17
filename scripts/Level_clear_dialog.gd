59805f2f7f33dd9db03ecc58782f06fddb1fbb4c
Eric Andrade <e.andrade5f@gmail.com>
2023-05-17 10:19:02 -0300

    feat: adding key control for pause menu

--- scripts/Level_clear_dialog.gd
+++ scripts/Level_clear_dialog.gd
@@ -2,9 +2,16 @@ extends Node2D
 
 @onready var Game = get_node("/root/World")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	if(visible && Input.is_action_just_pressed("shoot")):
+var exit_key: String = "shoot"
+
+func handle_exit_key_press():
+	if(exit_key!= "shoot" && exit_key != "escape"):
+		push_warning("warning: invalid exit key for pause menu")
+	if(visible && Input.is_action_just_pressed(exit_key)):
 		visible = false
 		get_tree().paused = false
 		Game.set_next_stage()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	handle_exit_key_press()
